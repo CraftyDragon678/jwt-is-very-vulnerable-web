3f8572ed65f614676ba90f4ea58aa1bf167e6989
CraftyDragon678 <craftydragon678@gmail.com>
2021-11-01 10:01:37 +0900

    feat: global style swr confgi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from 'next/app';
+import GlobalStyle from '../components/GlobalStyle';
+import { SWRConfig } from 'swr';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SWRConfig>
+      <GlobalStyle />
+      <Component {...pageProps} />;
+    </SWRConfig>
+  );
 }
 
 export default MyApp;
