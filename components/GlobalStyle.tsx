a88ea0096a1f3e4808a06d786f8ac7fb7d892b05
CraftyDragon678 <craftydragon678@gmail.com>
2021-11-01 10:15:34 +0900

    feat: font

--- components/GlobalStyle.tsx
+++ components/GlobalStyle.tsx
@@ -3,6 +3,10 @@ import normalize from 'styled-normalize';
 
 const GlobalStyle = createGlobalStyle`
   ${normalize};
+
+  body {
+    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
 `;
 
 export default GlobalStyle;
