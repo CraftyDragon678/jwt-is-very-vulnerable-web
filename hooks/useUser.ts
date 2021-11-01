36f9716d6f99f5d08bb382b45c1afc423474553c
CraftyDragon678 <craftydragon678@gmail.com>
2021-11-01 10:15:46 +0900

    feat: use user

---
+++ hooks/useUser.ts
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+import Router from 'next/router';
+import useSWR from 'swr';
+
+export interface User {
+  _id: string;
+  nickname: string;
+  type: string;
+}
+
+export default function useUser({
+  redirectTo = '/login',
+  redirectIfFound = false,
+  requiredAdmin = false,
+} = {}) {
+  const { data: user, mutate: mutateUser } = useSWR<User | null>('/users/me');
+
+  useEffect(() => {
+    // if no redirect needed, just return (example: already on /dashboard)
+    // if user data not yet there (fetch in progress, logged in or not) then don't do anything yet
+    if (!redirectTo || user === undefined) return;
+
+    if (
+      // If redirectTo is set, redirect if the user was not found.
+      (redirectTo && !redirectIfFound && !user) ||
+      // If redirectIfFound is also set, redirect if the user was found
+      (redirectIfFound && user) ||
+      (requiredAdmin && user?.type !== 'admin')
+    ) {
+      Router.push(redirectTo);
+    }
+  }, [user, redirectIfFound, redirectTo, requiredAdmin]);
+
+  return { user, mutateUser };
+}
